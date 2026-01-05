9de6380fd31835073c11e2d73ee1188494493784
Vivek-Mehra298 <vivekmehra298@gmail.com>
2026-01-05 17:27:56 +0530

    Refactor server.js with ES6 imports and DB file handling
    Refactor server.js to use ES6 imports and add file handling for database updates.

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-const jsonServer = require("json-server");
+import jsonServer from "json-server";
+import fs from "fs";
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
+
+const DB_FILE = "db.json";
+
+server.use((req, res, next) => {
+  if (["POST", "PUT", "PATCH", "DELETE"].includes(req.method)) {
+    const data = JSON.parse(fs.readFileSync(DB_FILE, "utf8"));
+    res.on("finish", () => {
+      fs.writeFileSync(DB_FILE, JSON.stringify(router.db.getState(), null, 2));
+    });
+  }
+  next();
+});
+
 server.use(router);
 
 server.listen(process.env.PORT || 3001);
